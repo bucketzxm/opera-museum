341c72673848e413f65aa17923b5b78f67805f02
SimonZheng <bucketzxm@gmail.com>
2015-05-17 15:43:58 +0800

    add entry, tag, image models

--- opera_museum/museum/admin.py
+++ opera_museum/museum/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from models import Entry, Tag, Image
+
+
+admin.site.register(Entry)
+admin.site.register(Image)
+admin.site.register(Tag)
+
+
--- opera_museum/museum/models.py
+++ opera_museum/museum/models.py
@@ -1,3 +1,54 @@
+#!-*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    image_url = models.FileField(verbose_name="图片地址")
+    description = models.TextField(blank=True, verbose_name="图片描述")
+
+    def __str__(self):
+        return u'图片 %s' % self.description
+
+    def __unicode__(self):
+        return u'图片 %s' % self.description
+
+    class Meta:
+        verbose_name = "图片"
+        verbose_name_plural = "图片"
+
+class Tag(models.Model):
+    key = models.CharField(max_length=256, verbose_name="属性名字")
+    value = models.CharField(max_length=256, verbose_name="属性内容")
+
+    def __str__(self):
+        return u'图片 %s' % self.description
+
+    def __unicode__(self):
+        return u'图片 %s' % self.description
+
+    class Meta:
+        verbose_name = "属性"
+        verbose_name_plural = "属性"
+
+class Entry(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="词条Id")
+    name = models.CharField(max_length=128, verbose_name="词条名称")
+    content = models.TextField(blank=True, verbose_name="词条内容")
+    images = models.ManyToManyField(Image, blank=True, verbose_name="图片")
+    video_url = models.TextField(blank=True, verbose_name="视屏链接")
+    relate_entry = models.TextField(blank=True, verbose_name="相关词条")
+    Tag = models.ManyToManyField(Tag, blank=True, verbose_name="属性分类")
+
+
+    def __str__(self):
+        return u'词条 %s : %s ' % ( str(self.id), self.name )
+
+    def __unicode__(self):
+        return u'词条 %s : %s ' % ( str(self.id), self.name )
+
+
+    class Meta:
+        verbose_name = "词条"
+        verbose_name_plural = "词条"
+
