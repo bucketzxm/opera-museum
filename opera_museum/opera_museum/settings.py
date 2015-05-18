8b11dba2bd87e6ce38191a47a7fbe5c737ed7e61
SimonZheng <bucketzxm@gmail.com>
2015-05-18 19:24:36 +0800

    include bootstrap and add footer css , base.html"

--- opera_museum/opera_museum/settings.py
+++ opera_museum/opera_museum/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'APP_DIRS' : True,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -99,7 +99,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-#LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
 LANGUAGE_CODE = "zh_cn"
 
 TIME_ZONE = 'UTC'
@@ -115,4 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
