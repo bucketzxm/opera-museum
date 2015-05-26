5dc3f52f28f2d91489216be784b13b0cb26602da
SimonZheng <bucketzxm@gmail.com>
2015-05-26 22:14:45 +0800

    now we can add Image in Entry

--- opera_museum/museum/admin.py
+++ opera_museum/museum/admin.py
@@ -13,12 +13,19 @@ from kombu.transport.django import models as kombu_models
 # Here we do some operatino before change entry in the admin site
 from museum.tasks import add_related_entries, delete_relate_entries
 
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
 class EntryAdmin(admin.ModelAdmin):
+
+    inlines = [
+        ImageInline,
+    ]
+
     def save_model(self, request, obj, form, change):
         obj.save()
         add_related_entries.delay(obj)
-
-
 
     def delete_model(self, request, obj):
         # obj.entry = request.entry
