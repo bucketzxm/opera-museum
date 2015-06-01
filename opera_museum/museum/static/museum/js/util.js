a640a64e22542dfbafbac40b63a501da944fca7b
Adam Cavendish <GetbetterABC@yeah.net>
2015-06-01 21:34:39 +0800

    Fix waterfall image out of border issue; waterfall content's font-size

--- opera_museum/museum/static/museum/js/util.js
+++ opera_museum/museum/static/museum/js/util.js
@@ -7,7 +7,7 @@ function Waterfall_addSizeConstraintToImage($html, restraintWidth) {
         var $node = $(node);
         var $img = $node.find('.museum_waterfall_entry_image');
         $node.css('width', restraintWidth);
-        $img.css('max-width', restraintWidth);
+        $img.css('max-width', restraintWidth-2);
         $img.css('height', 'auto');
 
         retHTML += $node.get(0).outerHTML + '\n';
